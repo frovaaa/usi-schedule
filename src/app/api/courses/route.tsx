9d35a4dbf140b75357db51edf4dcd3e68015d747
Davide Frova <frovad@usi.ch>
2024-09-10 12:36:52 +0200

    Added smart filter for courses based on semester SP-SA

--- src/app/api/courses/route.tsx
+++ src/app/api/courses/route.tsx
@@ -12,11 +12,36 @@ export async function GET(req: NextRequest) {
 
   try {
     const courses = await getCachedCourses(parseInt(educationId, 10));
-    const formattedCourses: Course[] = courses.map((course: any) => ({
-      id: course.id,
-      name_en: course.name_en || course.name_it,
-      semester_academic_year: course.semester_academic_year,
-    }));
+    const formattedCourses: Course[] = courses
+      .filter((course: any) => {
+        const currentYearSubstring = new Date().getFullYear().toString();
+        const currentMonth = new Date().getMonth() + 1; // Adding 1 to get the month from 1 to 12
+        const isSpringSemester = currentMonth >= 2 && currentMonth <= 7; // Assuming spring semester is from February to July
+        const isAutumnSemester = !isSpringSemester; // Assuming autumn semester is from August to January
+
+        if (isSpringSemester) {
+          // Filter in only the ones with the current year and SP (spring) as a substring
+          return (
+            course.semester_academic_year.includes(currentYearSubstring) &&
+            course.semester_academic_year.includes('SP')
+          );
+        } else if (isAutumnSemester) {
+          // Filter in only the ones with the next year and SA (autumn) as a substring
+          const nextYearSubstring = (new Date().getFullYear() + 1).toString();
+          return (
+            course.semester_academic_year.includes(nextYearSubstring) &&
+            course.semester_academic_year.includes('SA')
+          );
+        } else {
+          // If the current month is not within the expected range, exclude the course
+          return false;
+        }
+      })
+      .map((course: any) => ({
+        id: course.id,
+        name_en: course.name_en || course.name_it,
+        semester_academic_year: course.semester_academic_year,
+      }));
 
     return NextResponse.json(formattedCourses, {
       headers: {
