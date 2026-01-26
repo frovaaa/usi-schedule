7457be5cf0d86b838791c6b76153307b54545a3b
Davide Frova <frovad@usi.ch>
2026-01-26 11:14:29 +0100

    Unlocked all semesters

--- src/app/api/courses/route.tsx
+++ src/app/api/courses/route.tsx
@@ -14,31 +14,6 @@ export async function GET(req: NextRequest) {
     const courses = await getCachedCourses(parseInt(educationId, 10));
     const formattedCourses: Course[] = courses
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      .filter((course: any) => {
-        const currentYearSubstring = new Date().getFullYear().toString();
-        const currentMonth = new Date().getMonth() + 1; // Adding 1 to get the month from 1 to 12
-        const isSpringSemester = currentMonth >= 2 && currentMonth <= 7; // Assuming spring semester is from February to July
-        const isAutumnSemester = !isSpringSemester; // Assuming autumn semester is from August to January
-
-        if (isSpringSemester) {
-          // Filter in only the ones with the current year and SP (spring) as a substring
-          return (
-            course.semester_academic_year.includes(currentYearSubstring) &&
-            course.semester_academic_year.includes('SP')
-          );
-        } else if (isAutumnSemester) {
-          // Filter in only the ones with the next year and SA (autumn) as a substring
-          const nextYearSubstring = (new Date().getFullYear() + 1).toString();
-          return (
-            course.semester_academic_year.includes(nextYearSubstring) &&
-            course.semester_academic_year.includes('SA')
-          );
-        } else {
-          // If the current month is not within the expected range, exclude the course
-          return false;
-        }
-      })
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       .map((course: any) => ({
         id: course.id,
         name_en: course.name_en || course.name_it,
